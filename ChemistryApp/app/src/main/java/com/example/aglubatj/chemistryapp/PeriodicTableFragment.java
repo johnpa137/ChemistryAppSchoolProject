b7d80056ee3934d811ebe68d905edfa786857cca
RothranAgnos <rothran.agnos1218@gmail.com>
2016-03-14 14:57:54 -0700

    Added Fragment Documentation

--- ChemistryApp/app/src/main/java/com/example/aglubatj/chemistryapp/PeriodicTableFragment.java
+++ ChemistryApp/app/src/main/java/com/example/aglubatj/chemistryapp/PeriodicTableFragment.java
@@ -17,17 +17,33 @@ public class PeriodicTableFragment extends Fragment {
     public int cellSize;
     public int cellSymbolSize;
 
+    /**
+     * Constructor.
+     */
     public PeriodicTableFragment() {
         cellSize = 1;
         cellSymbolSize = 1;
     }
 
+    /**
+     * onCreateView for fragment classes
+     *
+     * @param inflater layout inflater
+     * @param container layout container
+     * @param savedInstanceState bundle savedinstancestate
+     * @return inflated view
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_periodic_table, container, false);
     }
 
+    /**
+     * onActivity created for fragment classes, sets listeners for element views
+     *
+     * @param savedInstanceState bundle saved state
+     */
     public void onActivityCreated(Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
         PeriodicTable pTable = PeriodicTable.getPeriodicTable();
@@ -42,12 +58,18 @@ public class PeriodicTableFragment extends Fragment {
         }
     }
 
+    /**
+     * Android onResume method, resizes element views to fit zoom
+     */
     @Override
     public void onResume(){
         super.onResume();
         updateCellDimensions();
     }
 
+    /**
+     * Method to resize element views
+     */
     private void updateCellDimensions(){
         PeriodicTable pTable = PeriodicTable.getPeriodicTable();
         for(int i = 0; i < PeriodicTable.NUMBER_OF_ELEMENTS; ++i){
@@ -60,12 +82,22 @@ public class PeriodicTableFragment extends Fragment {
         }
     }
 
+    /**
+     * Method to set dimensions of element views from outside the class, used by parent activity
+     *
+     * @param size size of the cell
+     * @param symbolSize size of the symbol on the cell
+     */
     public void setCellDimensions(int size, int symbolSize){
         cellSize = size;
         cellSymbolSize = symbolSize;
         updateCellDimensions();
     }
 
+    /**
+     * Listener for the element views
+     * @param view the view that was pressed
+     */
     public void onClickElementView(View view){
         PeriodicTableActivity pTAct = (PeriodicTableActivity) getActivity();
         if(pTAct.chkElementDetails.isChecked()){
